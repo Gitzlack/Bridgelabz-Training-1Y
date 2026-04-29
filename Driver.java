e78580785ed46613a449638d787f28f1e6f3d2d1
Utkrisht Srivastava <utkrisht.srivastava_cs25@gla.ac.in>
2026-04-29 11:25:33 +0530

    Add files via upload

---
+++ Driver.java
@@ -0,0 +1,11 @@
+package RouterProject;
+
+class Driver {
+    String id, name;
+    RouteLinkedList route = new RouteLinkedList();
+
+    Driver(String id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+}
---
+++ FuelCheckpoint.java
@@ -0,0 +1,17 @@
+package RouterProject;
+class FuelCheckpoint extends Checkpoint {
+
+    FuelCheckpoint(String id, String loc, double dist, double exp, double act) {
+        super(id, loc, dist, exp, act);
+    }
+
+    String getType() {
+        return "Fuel";
+    }
+
+    double calculatePenalty() {
+        if (isDelayed())
+            return 10;
+        return 0;
+    }
+}
